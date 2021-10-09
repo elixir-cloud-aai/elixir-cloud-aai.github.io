fc70cd9c2052f26c16fb1a54620e4bb21a644f74
git-anurag-hub <aguptaking@gmail.com>
2021-10-10 03:33:05 +0530

    Working Client to Server Api in Prod

--- pages/products.js
+++ pages/products.js
@@ -3,21 +3,30 @@ import { server } from "../config";
 import axios from "axios";
 import ProductsComponent from "../components/Products";
 
-const Products = () => {
+const Products = ({ products }) => {
   return (
     <>
       <Head>
         <title>Products</title>
       </Head>
-      <ProductsComponent></ProductsComponent>
+      <ProductsComponent products={products}></ProductsComponent>
     </>
   );
 };
 
 export const getStaticProps = async () => {
-  return {
-    props: {},
-  };
+  try {
+    const { data } = await axios.get(`${server}/api/products`);
+    console.log("data", data);
+
+    return {
+      props: {
+        products: data,
+      },
+    };
+  } catch (e) {
+    console.log("error", e);
+  }
 };
 
 export default Products;
